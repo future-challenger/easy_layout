d4a6fbc60e2cf97f30df117924848bc78a67c032
future-challenger <sunshine_anycall@hotmail.com>
2015-07-06 11:32:21 +0800

    first commit

---
+++ EasyLayout/EasyLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyLayout
+//
+//  Created by Bruce Lee on 7/6/15.
+//  Copyright (c) 2015 Bruce Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyLayout/EasyLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyLayout
+//
+//  Created by Bruce Lee on 7/6/15.
+//  Copyright (c) 2015 Bruce Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
